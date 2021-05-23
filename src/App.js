3e5f3d31a9f26e2d2e8816ecd1f873d66ec41854
Himanshu kumar <63831408+chrismysk506@users.noreply.github.com>
2021-05-24 02:22:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Video from './Video';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='videoscontainer'> 
+      <Video url='https://res.cloudinary.com/dlxwzl9nr/video/upload/v1621789813/tiktok_gxocjf.mp4' channel="ji"song="jo"descript="lo"likes={988}msgs={87}shares={123}></Video>
+      <Video url='https://res.cloudinary.com/dlxwzl9nr/video/upload/v1621789813/tiktok_gxocjf.mp4' channel="ji"song="jo"descript="lo"likes={988}msgs={87}shares={123}></Video>
+      
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
